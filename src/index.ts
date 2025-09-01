4e61ce2b52fd7850d5e793b1bc025087cbc603cd
ThulinaWickramasinghe <thulinawickramasinghe@gmail.com>
2025-09-01 09:29:57 +0530

    feat: add initial code to measure web-vitals

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Thulina");
+import { onCLS, onINP, onLCP } from "web-vitals";
+import type { Metric } from "web-vitals";
+
+console.log("Argus Initiated");
+
+const sendToAnalytics = (metric: Metric) => {
+  const body = JSON.stringify({
+    agent: "Argus",
+    ...metric
+  });
+
+  console.log("Argus log: ", metric);
+
+  // Use `navigator.sendBeacon()` to send the data, which supports
+  // sending while the page is unloading.
+  navigator.sendBeacon("/analytics", body);
+};
+
+onCLS(sendToAnalytics);
+onINP(sendToAnalytics);
+onLCP(sendToAnalytics);
