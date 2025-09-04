77151b00b98ff259e12f72a666ef465798d58856
ThulinaWickramasinghe <thulinawickramasinghe@gmail.com>
2025-09-04 19:20:05 +0530

    feat(TECH-4924): add config loading functionality

---
+++ src/configs/defaults.ts
@@ -0,0 +1,5 @@
+import type { ArgusConfig } from "../types/index";
+
+export const defaultConfigs: ArgusConfig = {
+  samplingRate: 0.5
+};
---
+++ src/configs/index.ts
@@ -0,0 +1,22 @@
+import { cosmiconfig } from "cosmiconfig";
+import { defaultConfigs } from "./defaults";
+
+const loadConfigs = async () => {
+  const explorer = cosmiconfig("argus");
+
+  try {
+    const result = await explorer.search();
+
+    if (!result) {
+      console.warn("No config file found. Using defaults.");
+      return defaultConfigs;
+    }
+
+    return result.config;
+  } catch (error) {
+    console.error("Failed to loead configs. Using defaults.: ", error);
+    return defaultConfigs;
+  }
+};
+
+export { loadConfigs };
