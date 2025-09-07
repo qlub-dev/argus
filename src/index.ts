1d16214c102576d67c5aea6c3e550c34a53b7b27
ThulinaWickramasinghe <thulinawickramasinghe@gmail.com>
2025-09-07 22:36:17 +0530

    refactor(web-vitals): rename methods to make it sound more generic

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { onCLS, onINP, onLCP } from "web-vitals";
+import { onCLS, onINP, onLCP, onFCP, onTTFB } from "web-vitals";
 import type { Metric } from "web-vitals";
 import type { TransporterCallback } from "./types";
 
 console.log("Argus Initiated");
 
-const METRIC_HANDLERS = [onCLS, onINP, onLCP];
+const METRIC_HANDLERS = [onCLS, onINP, onLCP, onFCP, onTTFB];
 
 const prepareMetric = (metric: Record<string, any>, metadata?: Record<string, any>) => {
   return {
@@ -16,14 +16,14 @@ const prepareMetric = (metric: Record<string, any>, metadata?: Record<string, an
   };
 };
 
-const generateTransporter =
+const generateReportHandler =
   (transporterCallback: TransporterCallback, metadata?: Record<string, any>) => (metric: Metric) => {
     const metricPayload = prepareMetric(metric, metadata);
     console.log("Argus log: ", metric);
     transporterCallback(metricPayload);
   };
 
-export const reportWebVitals = (transporter: TransporterCallback, metadata?: Record<string, any>) => {
-  const transportMetric = generateTransporter(transporter, metadata);
-  METRIC_HANDLERS.forEach((register) => register(transportMetric));
+export const reportWebVitals = (onReport: TransporterCallback, metadata?: Record<string, any>) => {
+  const reportHandler = generateReportHandler(onReport, metadata);
+  METRIC_HANDLERS.forEach((register) => register(reportHandler));
 };
