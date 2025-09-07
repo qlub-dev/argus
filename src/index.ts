4e096ee68668584c60174dbcadcb4be9b7c352c7
ThulinaWickramasinghe <thulinawickramasinghe@gmail.com>
2025-09-07 18:23:18 +0530

    feat(web-vitals): allow passing of metadata and refactor code

--- src/index.ts
+++ src/index.ts
@@ -4,27 +4,26 @@ import type { TransporterCallback } from "./types";
 
 console.log("Argus Initiated");
 
-const prepareMetric = (metric: Record<string, any>) => {
+const METRIC_HANDLERS = [onCLS, onINP, onLCP];
+
+const prepareMetric = (metric: Record<string, any>, metadata?: Record<string, any>) => {
   return {
     agent: "argus",
     event: "performance_metric",
     preparedAt: new Date().getTime(),
-    ...metric
+    ...metric,
+    ...(metadata ?? {})
   };
 };
 
-const generateTransporter = (transporterCallback: TransporterCallback) => (metric: Metric) => {
-  const metricPayload = prepareMetric(metric);
+const generateTransporter =
+  (transporterCallback: TransporterCallback, metadata?: Record<string, any>) => (metric: Metric) => {
+    const metricPayload = prepareMetric(metric, metadata);
+    console.log("Argus log: ", metric);
+    transporterCallback(metricPayload);
+  };
 
-  console.log("Argus log: ", metric);
-
-  transporterCallback(metricPayload);
-};
-
-export const reportWebVitals = (transporter: TransporterCallback) => {
-  const transportMetric = generateTransporter(transporter);
-
-  onCLS(transportMetric);
-  onINP(transportMetric);
-  onLCP(transportMetric);
+export const reportWebVitals = (transporter: TransporterCallback, metadata?: Record<string, any>) => {
+  const transportMetric = generateTransporter(transporter, metadata);
+  METRIC_HANDLERS.forEach((register) => register(transportMetric));
 };
