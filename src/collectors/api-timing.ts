3b41a74aa969c5c1262693273df58188043d3bc4
ThulinaWickramasinghe <thulinawickramasinghe@gmail.com>
2025-09-08 11:19:09 +0530

    feat(collectors): add api timing collector

---
+++ src/collectors/api-timing.ts
@@ -0,0 +1,26 @@
+import { PerformanceEntryType } from "../enums";
+import { ObserverMgr } from "../lib/observer-mgr";
+
+export function createApiTimingCollector(regex: RegExp, callback: (entry: PerformanceResourceTiming) => void) {
+  const mgr = ObserverMgr.getInstance();
+
+  const handler = (entry: PerformanceEntry) => {
+    if (!(entry instanceof PerformanceResourceTiming)) return;
+
+    if (entry.initiatorType !== "fetch" && entry.initiatorType !== "xmlhttprequest") {
+      return;
+    }
+
+    if (!regex.test(entry.name)) {
+      return;
+    }
+
+    callback(entry);
+  };
+
+  mgr.observe(PerformanceEntryType.RESOURCE, handler);
+
+  return {
+    disconnect: () => mgr.disconnect(PerformanceEntryType.RESOURCE)
+  };
+}
