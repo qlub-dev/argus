5e55bde54fdd6084148a3387ee2389f575e6014f
ThulinaWickramasinghe <thulinawickramasinghe@gmail.com>
2025-09-08 12:36:31 +0530

    fix: add logs for monitoring

--- src/collectors/api-timing.ts
+++ src/collectors/api-timing.ts
@@ -4,16 +4,21 @@ import { ObserverMgr } from "../lib/observer-mgr";
 export function createApiTimingCollector(regex: RegExp, callback: (entry: PerformanceResourceTiming) => void) {
   const mgr = ObserverMgr.getInstance();
 
-  const handler = (entry: PerformanceEntry) => {
-    if (!(entry instanceof PerformanceResourceTiming)) return;
+  console.trace("api timing collector created");
 
+  const handler = (entry: PerformanceEntry) => {
+    console.trace("atc h 1 ", entry);
+    if (!(entry instanceof PerformanceResourceTiming)) return;
+    console.trace("atc h 2 ", entry);
     if (entry.initiatorType !== "fetch" && entry.initiatorType !== "xmlhttprequest") {
       return;
     }
+    console.trace("atc h 3 ", entry);
 
     if (!regex.test(entry.name)) {
       return;
     }
+    console.trace("atc h 4 ", entry);
 
     callback(entry);
   };
