e5a2d7ba3f69ebeacbe9d12a413e335dde2d64fb
ThulinaWickramasinghe <thulinawickramasinghe@gmail.com>
2025-09-08 12:22:00 +0530

    feat: add singleton to argus initiator

--- src/index.ts
+++ src/index.ts
@@ -6,24 +6,38 @@ import type { ArgusConfig } from "./types/configs";
 import { prepareMetric } from "./utils";
 
 export class Argus {
+  static #instance: Argus | null = null;
+
   #config!: ArgusConfig;
   #apiCollectors: { disconnect: () => void }[] = [];
   #onReport: OnReportCb;
 
-  constructor(onReport: OnReportCb) {
+  private constructor(onReport: OnReportCb) {
     this.#onReport = onReport;
+  }
+
+  static getInstance(onReport?: OnReportCb) {
+    if (!Argus.#instance) {
+      if (!onReport) {
+        throw new Error("Argus is not initialized yet. You must provide an onReport callback the first time.");
+      }
+      Argus.#instance = new Argus(onReport);
+    }
+    return Argus.#instance;
   }
 
   async init(metadata?: Record<string, any>) {
     this.#config = await loadConfigs();
 
+    // Web vitals
     if (this.#config.webVitals?.enabled) {
       reportWebVitals(this.#onReport, metadata);
     }
 
+    // API timing
     if (this.#config.apiTiming?.enabled && Array.isArray(this.#config.apiTiming.trackers)) {
-      this.#config.apiTiming.trackers.forEach((tracker: { regex: RegExp }) => {
-        const regex = new RegExp(tracker.regex);
+      this.#config.apiTiming.trackers.forEach((tracker: { regex: RegExp | string }) => {
+        const regex = tracker.regex instanceof RegExp ? tracker.regex : new RegExp(tracker.regex);
         const collector = createApiTimingCollector(regex, (entry) => {
           const payload = prepareMetric(entry, metadata);
           this.#onReport(payload);
