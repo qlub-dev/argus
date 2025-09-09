e6da98141a64d7f60a03642c0af5082154411071
ThulinaWickramasinghe <thulinawickramasinghe@gmail.com>
2025-09-09 11:49:32 +0530

    feat: add label to event name

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 export const prepareMetric = (metric: Record<string, any>, metadata?: Record<string, any>) => {
   return {
     agent: "argus",
-    event: "performance_metric",
+    event: `performance_metric${metadata?.label ? `-${metadata.label}` : ""}`,
     preparedAt: performance.now(),
     ...metric,
     ...(metadata ?? {})
