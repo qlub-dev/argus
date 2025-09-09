ee6100167ea701fc10ace92ab430a652013db3ac
ThulinaWickramasinghe <thulinawickramasinghe@gmail.com>
2025-09-09 12:12:33 +0530

    style(lib): obfiscate code to keep it clean

--- src/lib/union-regex.ts
+++ src/lib/union-regex.ts
@@ -1,13 +1,6 @@
 export const createUnionRegex = (regexes: RegExp[], flags?: string): RegExp => {
-  if (!Array.isArray(regexes) || regexes.length === 0) {
-    throw new Error("Provide an array of regexes");
-  }
-
-  // collect unique flags if not explicitly passed
+  if (!Array.isArray(regexes) || regexes.length === 0) throw new Error("Missing regex array");
   const combinedFlags = flags || [...new Set(regexes.map((r) => r.flags).join(""))].join("");
-
-  // combine sources with alternation
   const source = regexes.map((r) => `(?:${r.source})`).join("|");
-
   return new RegExp(source, combinedFlags);
 };
